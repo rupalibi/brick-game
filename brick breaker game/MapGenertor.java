19a71d500dd508a678bf4c921e4431876e9fb0ca
rupalibi <100551309+rupalibi@users.noreply.github.com>
2022-03-02 20:08:35 +0530

    Add files via upload

---
+++ brick breaker game/MapGenertor.java
@@ -0,0 +1,41 @@
+package brichBracker;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class MapGenertor {
+	public int map[][];
+	public int brickwidth;
+	public int brickHeight;
+	public int length;
+	public MapGenertor(int row, int col) {
+		map = new int [row][col];
+		for(int i = 0; i < map.length; i++) {
+			for(int j=0; j < map[0].length; j++) {
+				map[i][j] = 1;
+			}
+		}
+		brickwidth =540/col;
+		brickHeight=150/row;
+	}
+	public void draw(Graphics2D g) {
+		for(int i = 0; i < map.length; i++) {
+			for(int j=0; j < map[0].length; j++) {
+				if(map[i][j] > 0) {
+					g.setColor(Color.white);
+					g.fillRect(i * brickwidth + 80,i * brickHeight + 50, brickwidth,brickHeight);
+					g.setStroke(new BasicStroke(7));
+					g.setColor(Color.black);
+					g.drawRect(j * brickwidth + 80,i * brickHeight + 50, brickwidth,brickHeight);
+				}
+				
+			}
+		}
+		
+	}
+	public void setBrickValue(int value,int row,int col) {
+		map[row][col] = value;
+	}
+
+}
